134f1d90f9ab6c39cbca43baa703ca995ffd17ed
damphu1102 <damphu207@gmail.com>
2025-04-25 13:54:38 +0700

    updateCreateCinema

--- src/main/java/com/example/backendcinema/Dto/Cinema/CinemaDtoCreate.java
+++ src/main/java/com/example/backendcinema/Dto/Cinema/CinemaDtoCreate.java
@@ -1,10 +1,12 @@
 package com.example.backendcinema.Dto.Cinema;
 
 import com.example.backendcinema.entity.Cinema.LocationEnum;
+import com.example.backendcinema.entity.Cinema.StatusActivate;
 import lombok.Data;
 
 @Data
 public class CinemaDtoCreate {
     private String cinemaName;
     private LocationEnum locationEnum;
+    private StatusActivate statusActivate;
 }
--- src/main/java/com/example/backendcinema/Dto/Cinema/CinemaDtoUpdate.java
+++ src/main/java/com/example/backendcinema/Dto/Cinema/CinemaDtoUpdate.java
@@ -1,12 +1,14 @@
 package com.example.backendcinema.Dto.Cinema;
 
 import com.example.backendcinema.entity.Cinema.LocationEnum;
+import com.example.backendcinema.entity.Cinema.StatusActivate;
 import lombok.Data;
 
 @Data
 public class CinemaDtoUpdate {
     private String cinemaName;
     private LocationEnum locationEnum;
+    private StatusActivate statusActivate;
     private int cinemaId;
 
 }
